c9d5aaafa7072387532ebd15d67d0dc958237bfd
meganii <yuhei24@gmail.com>
2019-01-06 17:54:29 +0900

    update

--- src/templates/tagslinks.js
+++ src/templates/tagslinks.js
@@ -1,12 +1,37 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
-export default ({ pageContext: { id }} ) => {
+export default ({ pageContext: { id }, data}) => {
+
+  console.dir(data)
+  const tags = data.allPagesJson.edges.map(edge => {
+    return (
+      <div key={edge.node.id}>
+        <Link to={`/${edge.node.title}`}>{edge.node.title}</Link>>
+      </div>
+    )
+  });
+
   return (
     <Layout>
       <div>
         <h1>{id}</h1>
+        {tags}
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ($id: String) {
+    allPagesJson(filter: {tags: {in: [$id]}}) {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }
+`
