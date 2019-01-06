9a0fc31ead14c21bc30bfa15c48793e34cb47ab0
meganii <yuhei24@gmail.com>
2019-01-06 18:34:07 +0900

    fix

--- src/templates/tagslinks.js
+++ src/templates/tagslinks.js
@@ -3,21 +3,25 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default ({ pageContext: { id }, data}) => {
-
-  console.dir(data)
-  const tags = data.allPagesJson.edges.map(edge => {
+  let edges;
+  if (!data.allPagesJson || !data.allPagesJson.edges) {
+    edges = []
+  } else {
+    edges = data.allPagesJson.edges
+  }
+  const tags = edges.map(edge => {
     return (
       <div key={edge.node.id}>
         <Link to={`/${edge.node.title}`}>{edge.node.title}</Link>>
       </div>
     )
-  });
+  })
 
   return (
     <Layout>
       <div>
         <h1>{id}</h1>
-        {tags}
+        {tags ? tags : ''}
       </div>
     </Layout>
   )
