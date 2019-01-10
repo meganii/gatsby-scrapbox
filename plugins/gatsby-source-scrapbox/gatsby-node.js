5178d0d5d6faa08c989e920f15bcfc4e7aaadb52
meganii <yuhei24@gmail.com>
2019-01-11 06:25:57 +0900

    Refactoring

--- plugins/gatsby-source-scrapbox/gatsby-node.js
+++ plugins/gatsby-source-scrapbox/gatsby-node.js
@@ -1,54 +1,35 @@
-const axios = require('axios');
+const axios = require('axios')
+const crypto = require('crypto')
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest },
-  configOptions
+  { boundActionCreators: { createNode }, createNodeId, createContentDigest },
+  { plugins, ...options }
 ) => {
-  const { createNode } = actions
+  const client = axios.create({
+    baseURL: 'https://scrapbox.io/api/',
+    headers: {
+      Cookie: `connect.sid=${options.sid};`
+    }
+  })
+  const {data} = await client.request({
+    method: 'get',
+    url: `/pages/${options.project_name}`,
+  })
 
-  // Gatsby adds a configOption that's not needed for this plugin, delete it
-  delete configOptions.plugins
-  // Convert the options object into a query string
-  // const apiOptions = queryString.stringify(configOptions)
-  // Join apiOptions with the Pixabay API URL
-  const apiUrl = `https://scrapbox.io/api/pages/${configOptions.project_name}`
-
-  // Helper function that processes a photo to match Gatsby's node structure
-  const processPage = (page) => {
-    const nodeId = createNodeId(`scrapbox-page-${page.id}`)
-    const nodeContent = JSON.stringify(page)
-    const nodeData = Object.assign({}, page, {
-      id: nodeId,
+  data.pages.forEach(page => {
+    createNode({
+      ...page,
+      id: createNodeId(`scrapbox-page-${page.id}`),
       parent: null,
       children: [],
       internal: {
-        type: `ScrapboxPage`,
-        content: nodeContent,
-        contentDigest: createContentDigest(nodeContent),
-      },
-    })
-
-    return nodeData
-  }
-
-  // Gatsby expects sourceNodes to return a promise
-  try {
-    const client = axios.create({
-      baseURL: 'https://scrapbox.io/api/',
-      headers: {
-        Cookie: `connect.sid=${configOptions.sid};`
+        type: 'ScrapboxPage',
+        content: JSON.stringify(page),
+        contentDigest: crypto
+          .createHash('md5')
+          .update(JSON.stringify(page))
+          .digest('hex')
       }
     })
-    const {data} = await client.request({
-      method: 'get',
-      url: `/pages/${configOptions.project_name}`,
-    })
-
-    data.pages.forEach(page => {
-      const nodeData = processPage(page)
-      return createNode(nodeData)
-    })
-  } catch (error) {
-    console.log(error);
-  }
+  })
 }
