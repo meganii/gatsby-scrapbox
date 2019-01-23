0b454a4c9fa5dd4938177e0888d6c383055ddf09
meganii <yuhei24@gmail.com>
2019-01-23 22:42:08 +0900

    add plugin

--- src/templates/scrapbox-page.js
+++ src/templates/scrapbox-page.js
@@ -7,10 +7,18 @@ export default ({ pageContext: { id }, data}) => {
   const content = (lines) => {
     let content = ''
     lines.forEach(line => {
-      content = content + line.text
+      content = content + `<p>${replaceTags(line.text)}</p>`
     })
     return content
   }
+
+  const replaceTags = (line) => {
+    let html = line.replace(/#([^\s$]*)/g, '<a href="/$1">$1</a>')
+    html = html.replace(/\[(https:\/\/.+?\.(jpg|jpeg|png)|https:\/\/gyazo\.com.+?)\s*(https:\/\/.+?)?\]/, '<img src="$1" />')
+    html = html.replace(/\[(.+?)\]/g, '<a href="/$1">$1</a>')
+    return html
+  }
+
   return (
     <Layout>
       <div>
