5a3f0f7aab84e7132256c5726f6863e367d178de
meganii <yuhei24@gmail.com>
2019-05-01 20:46:39 +0900

    Add emotion lib

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,12 @@ module.exports = {
         project_name: "mediamarker-icossa",
       },
     },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
